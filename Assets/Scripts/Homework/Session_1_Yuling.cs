440774d0811f14d7bf738ff0b4e5fe01320e64f6
YulingGuo007 <73468463+YulingGuo007@users.noreply.github.com>
2020-11-01 21:06:47 +0000

    Add files via upload

---
+++ Assets/Scripts/Homework/Session_1_Yuling.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Session_1_Yuling : MonoBehaviour
+{
+    // 1. variables
+
+    public int myFirstWholeNumber = 1;
+    private int mySecondWholeNumber = 99;
+    
+    public float myFirstDecimalNumber = 1.1234567777777777f;
+
+    public double myFirstLongDecimalNumber = 1.12345677777777771234567777777777d;
+
+    private string myFirstEords = "Hello everyone! Happy to work with you!";
+
+    public string mySecondWord = "Hello!";
+
+    private bool myFirstBoolean = false;
+    public bool mySecondBoolean = true;
+    // Array
+    public int[] myWholeNumbersCollection = new int[5];
+    public float[] myDecimalNumbersCollection = { 1.3f, 2.5f, 3.0f };
+
+    //Lists
+    public List<int> myFirstIntList = new List<int>();
+
+    // 2. functions
+  
+    private float CalculatorSum (float _FirstNumber, float _SecondNumber)
+    {
+        float SumResult = _FirstNumber + _SecondNumber;
+        return SumResult;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        float SumResult = CalculatorSum(myFirstWholeNumber, mySecondWholeNumber);
+        System.Diagnostics.Debug.Log("Adding two numbers result is: " + SumResult.ToString());
+
+        myDecimalNumbersCollection[0] = 2.8f;
+        float SumRult2 = CalculatorSum(myDecimalNumbersCollection[0], myDecimalNumbersCollection[2]);
+
+        myFirstIntList.Add(3);
+        myFirstIntList.Add(9);
+        myFirstIntList[0] = 5;
+        myFirstIntList[1] = 12;
+
+        System.Diagnostics.Debug.Log(myFirstIntList[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Homework/Session_2_Yuling.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Session_2_Yuling : MonoBehaviour
+{
+    // Colour Names Array
+    string[] colours = { "red", "green", "blue", "yellow", "brown" };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug Log(colours[1]); 
+
+        // LOOPS
+        // For loop(counter, end condition, step)
+
+        for (int counter = 0; counter < colours .Length; counter = counter +1)
+        {
+            Log("The current color at pos" + counter);
+            Log(" " + colours[counter]);
+        }
+
+        int sum0f0ddNumber = 0;
+        for (int i = 1; i < 100, i = i + 2 )
+        {
+            sum0f0ddNumber += i;
+        }
+        Log("All odd numbers to 100 added together is:" + sum0f0ddNumber);
+
+        //Shorthand for loop
+        foreach (string currentColour in colours )
+        {
+            Log("Going over the rainbow with " + currentColour);
+        }
+        // While loop (needs a condition to end)
+        int whileCounter = 0;
+        string rainbow = "Rainbow colours: ";
+        while (whileCounter < colours .Length )
+        {
+            rainbow += colours[whileCounter] + ",";
+            whileCounter += 2;
+            
+        }
+        Log(rainbow);
+
+        // CONDITIONALS (asking the computer questions)
+
+        // If statement (has 2 branches, one for true, one for false, false branch is optional)
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Log("Whole number:" + i);
+            }
+            else
+            {
+                Log("Odd number" + i);
+            }
+        }
+
+        // Asking multiple questions
+        bool question1Answer = true;
+        bool question2Answer = true;
+        bool question3Answer = false;
+
+        // Two conditions met (Concatenation)
+
+        if (question1Answer && question2Answer )
+        {
+            Log("Both answers are true");
+        }
+
+        // More than 2 questions
+        if (question1Answer || question2Answer || question3Answer)
+        {
+            // Action here
+        }
+
+        // Question in question
+        if(question1Answer == true)
+        {
+            if (question2Answer == true)
+            {
+                if (question3Answer == false)
+                {
+                    // Action here
+                }
+            }
+        }       
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
